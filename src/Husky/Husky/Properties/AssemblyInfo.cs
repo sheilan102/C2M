b20fac59fba597e321da4217b37812fefce0cef3
Sheilan <sheilan102@yahoo.com>
2019-05-01 05:33:39 +0300

    ui

--- src/Husky/Husky/Properties/AssemblyInfo.cs
+++ src/Husky/Husky/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Husky - Call of Duty BSP Extractor")]
-[assembly: AssemblyDescription("Extracts BSP Data from Call of Duty")]
+[assembly: AssemblyTitle("C2M - Call of Duty BSP Extractor")]
+[assembly: AssemblyDescription("Call of Duty BSP Extractor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scobalula")]
-[assembly: AssemblyProduct("Husky")]
-[assembly: AssemblyCopyright("Copyright © Scobalula  2018")]
+[assembly: AssemblyCompany("Philip/Scobalula/SHEILAN")]
+[assembly: AssemblyProduct("HuskyUI")]
+[assembly: AssemblyCopyright("Copyright © SHEILAN 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,8 +19,25 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bbeb0978-d656-4432-80e9-cf2083f53349")]
+//In order to begin building localizable applications, set
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
